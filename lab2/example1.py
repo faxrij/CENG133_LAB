670415e66af59a92990239a16d7939a75ec5bf1e
h15688631 <cfaxri@mail.ru>
2020-11-09 08:58:45 +0000

    leap year

--- lab2/example1.py
+++ lab2/example1.py
@@ -1,10 +1,9 @@
-a = int(input("Enter the a value : "))
-b = int(input("Enter the b value : "))
-c = int(input("Enter the c value : "))
-d = (b**2 - 4*a*c)
-if d>0:
-  print("It has two real solutions")
-if d==0:
-  print("It has only one real solution")
-if d<0:
-  print("It does not have any real solutions")
+year = int(input())
+if year%100==0:
+    if year%400==0:
+        print(year,'is a leap year')
+
+elif year %4==0:
+        print(year,'is a leap year')
+else:
+    print('not leap')
