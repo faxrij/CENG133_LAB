75166731f92ea84568e96c497b1ccbc19ab6439a
h15688631 <cfaxri@mail.ru>
2020-11-16 09:10:39 +0000

    %of evens

--- lab2/example1.py
+++ lab2/example1.py
@@ -1,5 +1,10 @@
-number = 8
-table = 1
-for i in range(1,11):
-  table = number*i
-  print(table)
+evens=0
+numbers=int(input())
+for i in range(numbers):
+  number = int(input())
+  if number%2==0:
+    evens+=1
+  else:
+    pass
+percentage = (evens/numbers)*100
+print(percentage)
