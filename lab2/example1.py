e1f3067f57d493706b8dbb9a91e5ee4251bcb240
h15688631 <cfaxri@mail.ru>
2020-11-16 09:34:11 +0000

    password

--- lab2/example1.py
+++ lab2/example1.py
@@ -1,9 +1,10 @@
-number1 = int(input())
-number2 = int(input())
-match = 0
-while number1>0 and number2>=0:
-  if number1%10==number2%10:
-    match+=1
-  number1//=10
-  number2//=10
-print(match)
+password = 'abc123'
+while True:
+  enter = input("Enter a password ")
+  if enter=='help':
+    print('a')
+  elif enter==password:
+    print("Welcome ")
+  else:
+    print('Wrong ')
+  break
