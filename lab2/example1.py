7fe62ec76a4047b8e8d142cb43c38c708477826e
h15688631 <cfaxri@mail.ru>
2020-11-18 13:59:04 +0000

    password

--- lab2/example1.py
+++ lab2/example1.py
@@ -1,10 +1,10 @@
 password = 'abc123'
-while True:
-  enter = input("Enter a password ")
-  if enter=='help':
-    print('a')
-  elif enter==password:
-    print("Welcome ")
-  else:
-    print('Wrong ')
-  break
+your_password = input('Enter ')
+while your_password!=password:
+  print('your password is incorrect, try again')
+  your_password = input('Enter again: ')
+  if your_password=='help':
+    print(password[0])
+  elif your_password==password:
+    print('Yes')
+    break
