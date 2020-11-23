a6e644e2ef155fff37da25496533d7630019cc50
h15688631 <cfaxri@mail.ru>
2020-11-23 09:37:26 +0000

    grades

---
+++ lab5/example1.py
@@ -0,0 +1,10 @@
+students = int(input('Number of students: '))
+example = 1
+final = []
+while example<=students:
+  for i in range(3):
+    grades = input('grades :')
+    grades = (grades)
+    final.append(grades)
+  print(final)
+  example+=1
