d197efca7fd03ec2167e126dd5a7e078f3a8cc8a
h15688631 <cfaxri@mail.ru>
2020-11-30 08:49:18 +0000

    dictionary age problem

---
+++ lab/example2.py
@@ -0,0 +1,5 @@
+empty_list = [1,2,3,4,5,3,4]
+
+for i in empty_list:
+  if empty_list.count(i)>1:
+    print(i)
---
+++ lab7/example1.py
@@ -0,0 +1,4 @@
+name = {'John':15,'Ned':45,'Arya':9,'Catelyn':44,'Bran':10}
+for i,j in name.items():
+  if j >18:
+    print(i)
