a47a83f09bc4080e4f5b60925c40c58d5c08e5e6
h15688631 <cfaxri@mail.ru>
2020-12-14 18:37:23 +0000

    binary to dec, dec to binary

---
+++ lab8/example4.py
@@ -0,0 +1,31 @@
+def binary_to_dec(num1):
+ 
+  sum = 0
+  for i in range(len(num1)):
+    b = num1[-1]
+    c = int(b)*(2**i)
+    sum+=c
+    num1=int(num1)
+    num1//=10
+    num1 = str(num1)
+  return sum
+
+def dec_to_binary(num2):
+    number = 0
+    list_ = []
+    while True:
+        c = num2%2
+        list_.append(c)
+        num2//=2
+        if num2==0:
+            break
+    c = []
+    for i in range(len(list_)-1,-1,-1):
+        c.append(list_[i])
+    a = ''
+    for j in c:
+        j = str(j)
+        a+=j
+    return a
+print(binary_to_dec('10010'))
+print(dec_to_binary(18))
